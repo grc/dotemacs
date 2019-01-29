75adb9d2c2f50cc588c24fd3d502470451092c89
Giles Chamberlin <giles@pexip.com>
2019-01-29 12:16:32 +0000

    Customisation changes

--- custom.el
+++ custom.el
@@ -3,90 +3,60 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-engine (quote luatex))
- '(TeX-view-program-selection
-   (quote
-    (((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "xpdf")
-     (output-html "xdg-open"))))
- '(ansi-color-names-vector
-   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
- '(browse-url-browser-function (quote eww-browse-url))
- '(browse-url-generic-program "/Users/grc/bin/conkeror")
- '(canlock-password "7eb9fe24d5c7742671cba67f3eba35dd151d0cdc")
- '(compilation-message-face (quote default))
- '(custom-enabled-themes (quote (grc)))
  '(custom-safe-themes
-   (quote
-    ("479eba125f9e97a0208b642a99eee1d816fa208fe3a06f73e444504beb0b17f7" "144c32b0a2f17b78943bbb86217758ba6e62d75dd0623799679d2d08a008aa08" "7ed6913f96c43796aa524e9ae506b0a3a50bfca061eed73b66766d14adfa86d1" default)))
- '(display-time-world-list
-   (quote
-    (("America/Los_Angeles" "Los Angeles")
-     ("America/New_York" "New York")
-     ("Europe/London" "London")
-     ("Europe/Paris" "Paris")
-     ("Asia/Calcutta" "Bangalore")
-     ("Asia/Tokyo" "Tokyo"))))
- '(fci-rule-color "#49483E")
- '(gnus-ignored-from-addresses (quote ("giles@jujutsu\\.org\\.uk" "giles@pexip\\.com")))
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
- '(highlight-tail-colors
-   (quote
-    (("#49483E" . 0)
-     ("#67930F" . 20)
-     ("#349B8D" . 30)
-     ("#21889B" . 50)
-     ("#968B26" . 60)
-     ("#A45E0A" . 70)
-     ("#A41F99" . 85)
-     ("#49483E" . 100))))
- '(inhibit-splash-screen t)
- '(inhibit-startup-screen t)
- '(magit-use-overlays nil)
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(org-agenda-files
-   (quote
-    ("~/org/home.org" "~/org/calendar.org" "~/org/notes.org")))
- '(org-entities-user
-   (quote
-    (("space" "\\ " nil " " " " " " " ")
-     ("grcbreak" "\\\\" nil "" "" "" ""))))
- '(org-latex-pdf-process
-   (quote
-    ("lualatex -shell-escape -interaction nonstopmode -output-directory %o %f" "lualatex -shell-escape -interaction nonstopmode -output-directory %o %f" "lualatex -shell-escape -interaction nonstopmode -output-directory %o %f")))
- '(org-latex-to-pdf-process (quote ("LATEX='pdflatex -shell-escape' texi2dvi %f")))
+   '("/Users/grc/org/agenda.org" "/Users/grc/org/appointments.org" "/Users/grc/org/chef.org" "/Users/grc/org/dev-leads.org" "/Users/grc/org/devnotes.org" "/Users/grc/org/devnotes2.org" "/Users/grc/org/distance-calc.org" "/Users/grc/org/endpoint.org" "/Users/grc/org/facebook.org" "/Users/grc/org/fun-links.org" "/Users/grc/org/gdpr.org" "/Users/grc/org/guildford.org" "/Users/grc/org/home.org" "/Users/grc/org/janet.org" "/Users/grc/org/journal.org" "/Users/grc/org/musings.org" "/Users/grc/org/new-mac.org" "/Users/grc/org/new-office.org" "/Users/grc/org/notes.org" "/Users/grc/org/org-config.org" "/Users/grc/org/personal-todo.org" "/Users/grc/org/release-process.org" "/Users/grc/org/sarah.org" "/Users/grc/org/wag.org" "/Users/grc/org/zsh.org"))
  '(package-selected-packages
-   (quote
-    (crux bug-hunter request projectile gist haskell-emacs gnorb paradox org magit goto-chg use-package smartparens haskell-mode gh expand-region debbugs auctex ace-isearch ac-nrepl)))
- '(paradox-github-token t)
- '(syslog-debug-face
-   (quote
-    ((t :background unspecified :foreground "#A1EFE4" :weight bold))))
- '(syslog-error-face
-   (quote
-    ((t :background unspecified :foreground "#F92672" :weight bold))))
- '(syslog-hour-face (quote ((t :background unspecified :foreground "#A6E22E"))))
- '(syslog-info-face
-   (quote
-    ((t :background unspecified :foreground "#66D9EF" :weight bold))))
- '(syslog-ip-face (quote ((t :background unspecified :foreground "#E6DB74"))))
- '(syslog-su-face (quote ((t :background unspecified :foreground "#FD5FF0"))))
- '(syslog-warn-face
-   (quote
-    ((t :background unspecified :foreground "#FD971F" :weight bold))))
- '(weechat-color-list
-   (quote
-    (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
+   '(flycheck graphviz-dot-mode org2blog helm-projectile sudo-edit helm-ebdb blimp yaml-mode org-gcal yasnippet-classic-snippets bbdb define-word slime f3 dash go-mode shell-pop spacemacs-theme cider google-this which-key pp-c-l dired-narrow use-package tc spotlight smartparens slack skewer-mode restclient projectile pdf-tools ox-reveal origami org-plus-contrib magit helpful helm-dash haskell-mode gnuplot crux auctex))
+ '(python-shell-completion-native-enable nil)
+ '(safe-local-variable-values
+   '((eval setq-local org-html-postamble nil)
+     (eval setq-local lob
+           (concat site-root "lob.inc"))
+     (eval setq-local site-root
+           (locate-dominating-file default-directory ".dir-locals.el"))
+     (eval setq-local project-path
+           (locate-dominating-file default-directory ".dir-locals.el"))
+     (eval setq-local project-path
+           (locate-dominating-file defualt-directory ".dir-locals.el"))
+     (eval message "Project directory set to `%s'." project-path)
+     (eval setq-local project-path
+           (file-name-directory
+            (let
+                ((d
+                  (dir-locals-find-file ".")))
+              (if
+                  (stringp d)
+                  d
+                (car d)))))
+     (eval message "Project directory set to `%s'." my-project-path)
+     (eval set
+           (make-local-variable 'my-project-path)
+           (file-name-directory
+            (let
+                ((d
+                  (dir-locals-find-file ".")))
+              (if
+                  (stringp d)
+                  d
+                (car d)))))
+     (site-root
+      (eval nth 1
+            (s-split " "
+                     (pwd))))
+     (site-root eval
+                (nth 1
+                     (s-split " "
+                              (pwd))))
+     (site-root nth 1
+                (s-split " "
+                         (pwd)))
+     (org-confirm-babel-evaluate))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(erc-input-face ((t (:foreground "yellow"))))
- '(error ((t (:foreground "red1" :weight bold))))
- '(eshell-prompt ((t (:foreground "chartreuse" :weight normal))))
- '(flyspell-incorrect ((t (:foreground "gold"))))
- '(font-lock-function-name-face ((t (:foreground "aquamarine"))))
- '(org-todo ((t (:foreground "gold" :weight bold))))
- '(region ((t (:background "CadetBlue4")))))
+ '(font-lock-function-name-face ((t (:foreground "deep sky blue"))))
+ '(org-todo ((t (:foreground "OrangeRed1" :weight bold)))))
